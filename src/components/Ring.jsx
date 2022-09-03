1e9be35eb43d79017ebcb7236883e0b386d46031
Lurimago <luisricardomarcelogomez@gmail.com>
2022-09-03 02:59:44 -0500

    03-09-2022

--- src/components/Ring.jsx
+++ src/components/Ring.jsx
@@ -7,89 +7,87 @@ import imgGmail from "../img/gmail.png";
 import imgFacebook from "../img/facebook.png";
 import imgWhatsapp from "../img/whatsapp.png";
 import imgDownload from "../img/download.png";
-import imgGithub from "../img/github.png";
+import svgGithub from "../img/github.svg";
 
 const Ring = () => {
   const [displacementFilter, setDisplacementFilter] = useState();
   return (
-    <>
-    <div className="containgerRing">
-    <img className="myphoto"src={image} />
-    <div className="ring">
-      <svg>
-        <filter id="wave">
-          <feTurbulence
-            x="-40px"
-            y="-40px"
-            baseFrequency="0.02"
-            numOctaves="5"
-            seed="2"
-          >
-            <animate
-              attributeName="baseFrequency"
-              dur="15"
-              values="0.01 ; 0.02 ; 0.01"
-              repeatCount="indefinite"
-            />
-          </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" scale="30" />
-        </filter>
-      </svg>
-
-      <svg
-        className="hole"
-        width="200px"
-        height="200px"
-        viewBox="0 0 220 220"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <filter id="displacementFilter">
-          <feTurbulence
-            type="turbulence"
-            baseFrequency="0.05"
-            numOctaves="2"
-            result="turbulence"
-            seed="2"
-          >
-            <animate
-              attributeName="baseFrequency"
-              dur="15"
-              values="0.03 ; 0.05 ; 0.03"
-              repeatCount="indefinite"
-            />
-          </feTurbulence>
-          <feDisplacementMap
-            in2="turbulence"
-            in="SourceGraphic"
-            scale="50"
-            xChannelSelector="R"
-            yChannelSelector="G"
-          />
-        </filter>
-
-        <circle
-          cx="100"
-          cy="100"
-          r="100"
-          style={{ filter: `url(#${displacementFilter})` }}
-        />
-      </svg>
-    </div>
-
-    </div>
     <Container>
+      <div className="containgerRing">
+        <img className="myphoto" src={image} />
+        <div className="ring">
+          <svg>
+            <filter id="wave">
+              <feTurbulence
+                x="-40px"
+                y="-40px"
+                baseFrequency="0.02"
+                numOctaves="5"
+                seed="2"
+              >
+                <animate
+                  attributeName="baseFrequency"
+                  dur="15"
+                  values="0.01 ; 0.02 ; 0.01"
+                  repeatCount="indefinite"
+                />
+              </feTurbulence>
+              <feDisplacementMap in="SourceGraphic" scale="30" />
+            </filter>
+          </svg>
+
+          <svg
+            className="hole"
+            width="200px"
+            height="200px"
+            viewBox="0 0 220 220"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <filter id="displacementFilter">
+              <feTurbulence
+                type="turbulence"
+                baseFrequency="0.05"
+                numOctaves="2"
+                result="turbulence"
+                seed="2"
+              >
+                <animate
+                  attributeName="baseFrequency"
+                  dur="15"
+                  values="0.03 ; 0.05 ; 0.03"
+                  repeatCount="indefinite"
+                />
+              </feTurbulence>
+              <feDisplacementMap
+                in2="turbulence"
+                in="SourceGraphic"
+                scale="50"
+                xChannelSelector="R"
+                yChannelSelector="G"
+              />
+            </filter>
+
+            <circle
+              cx="100"
+              cy="100"
+              r="100"
+              style={{ filter: `url(#${displacementFilter})` }}
+            />
+          </svg>
+        </div>
+      </div>
+
       <div className="rectangle">
         <div className="rectangleSocialNetworks">
-           <img className="linkedin"src={imgLinkedin} />
-           <img className="download"src={imgDownload} />
-           <img className="gmail"src={imgGmail} />
-           <img className="facebook"src={imgFacebook} />
-           <img className="github"src={imgGithub} />
-           <img className="whatsapp"src={imgWhatsapp} />  
+          <img className="linkedin" src={imgLinkedin} />
+          <img className="svgGithub" src={svgGithub} />
+          <img className="download" src={imgDownload} />
+          <img className="facebook" src={imgFacebook} />
+          <img className="whatsapp" src={imgWhatsapp} />
+          <img className="gmail" src={imgGmail} />
         </div>
-        </div>
-        </Container>
-        </>
+      </div>
+    </Container>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Container } from "react-bootstrap";
 import "bootswatch/dist/simplex/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Container>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Container>
+  </React.StrictMode>
 );
